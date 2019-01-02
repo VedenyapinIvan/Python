8d65d0891695637d0cd4b09da75fcfc15b662c82
Ivan Vedenyapin <vedenyapin.ivan@yandex.ru>
2019-01-02 17:02:07 +0300

    correct script

--- statsSelection.py
+++ statsSelection.py
@@ -9,17 +9,19 @@ with postgresql.open('pq://postgres:admin@localhost:5432/MSED') as msed:
     surgery = 0  # реконструктивная хирургия, rhb_type id = 3
     prosthetics = 0  # протезирование, rhb_type id = 4
     while stack < count:
-        patients = msed.query('select * from prg order by id limit $1 offset $2', 100, stack)
+        patients = msed.query('select cfile from prg order by id limit $1 offset $2', 100, stack)
         for person in patients:
             file = person['cfile']
             if ('<IsForChild>false</IsForChild>' in file) and ('<IsFirst>false</IsFirst>' in file):
                 with postgresql.open('pq://postgres:admin@localhost:5432/MSE') as mse:
 
+                    # TODO use this for auto parsing
                     # rehabilitation += (mse.query('select count(*) from prg_rhb where prgid=$1 and resid=1 and typeid=2', person['id']))[0][0]
                     # surgery += (mse.query('select count(*) from prg_rhb where prgid=$1 and resid=1 and typeid=3', person['id']))[0][0]
                     # prosthetics += (mse.query('select count(*) from prg_rhb where prgid=$1 and resid=1 and typeid=4', person['id']))[0][0]
 
-                    actions = mse.query('select * from prg_rhb where prgid=$1 and resid=1', person['id'])
+                    # TODO usi this for unique parsing
+                    actions = mse.query('select typeid from prg_rhb where prgid=$1 and resid=1', person['id'])
                     for act in actions:
                         if act['typeid'] == 2:
                             rehabilitation += 1
